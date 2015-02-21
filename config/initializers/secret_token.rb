e86a9d9c84eb8a369b5bca5a07ecda0cb077c95e
Donald Chen <d.chen72@gmail.com>
2015-02-21 10:49:51 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWCInventoryTracker::Application.config.secret_token = '5c7859e35a7567a22c4ac8d2ee626cde41e9e7335d48c696dff3b8183c5184ccb880e2fa71e4d003976432f2915d3df0de45321339a082ca4728c502858eb6d1'
