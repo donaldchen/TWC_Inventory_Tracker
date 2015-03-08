7a5da7af00123754cf27320805cf54e82ef5340d
SamPaul94 <samuelpaul7@gmail.com>
2015-03-07 18:24:26 -0800

    Added code to login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,23 @@ class LoginController < ApplicationController
 	end
 
 	def login
+<<<<<<< HEAD
 		
+=======
+        client = Databasedotcom::Client.new("config/databasedotcom.yml")
+        client.authenticate :username => 'sampaul@berkeley.edu', :password => 'pleasedontkillme1TfPa8Eb0NneMsYA56IWaYDvd'
+        client.materialize("Contact")
+        contact = SFDC::Contact.find_by_email(email_address_field)
+        password = contact.Password__c
+
+        if password == password_field:
+        	# GRANT ACCESS
+        else:
+        	# DENY ACCESS
+>>>>>>> Added code to login controller
 	end
 end
+
+
+# Added Password field to Contact.
+# 
