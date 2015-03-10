5da13a14a2d7ee3dcdae3292328288cb130e1292
SamPaul94 <samuelpaul7@gmail.com>
2015-03-10 15:59:26 -0700

    Merge complete

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -20,26 +20,6 @@ class LoginController < ApplicationController
         redirect_to root_path
     end
 
-<<<<<<< HEAD
-=======
-=begin
-    def login
-        client = Databasedotcom::Client.new("config/databasedotcom.yml")
-        client.authenticate :username => 'sampaul@berkeley.edu', :password => 'pleasedontkillme1TfPa8Eb0NneMsYA56IWaYDvd'
-        client.materialize("Contact")
-        contact = Contact.find_by_email(params[:email])
-        password = contact.Password__c
-        if password == params[:password_field]
-        	flash[:notice] = "Login Successful"
-        else
-        	flash[:notice] = "Login not Successful"
-        end
-        
-        redirect_to root
-	end
-=end
-
->>>>>>> eb502c3f0c76c5cb029b11a797b3f0d2baaa0b57
 # Added Password field to Contact.
 # Connected App:        All users may self authorize, relax ip restrictions
 end
