7ab688aa7262f1b5fde5057b670ae90f30d93c0f
plzdonkillme <robinhu510@gmail.com>
2015-03-10 04:08:05 +0000

    Can connect to salesforce now

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,21 @@
 class LoginController < ApplicationController
+    include Databasedotcom::Rails::Controller
+
     def index
     end
 
+    def login
+        c = Contact.find_by_email(params[:email])
+        if c != nil
+        	flash[:notice] = c.Password__c
+        else
+        	flash[:notice] = "Login not Successful"
+        end
+        
+        redirect_to root_path
+    end
+
+=begin
     def login
         client = Databasedotcom::Client.new("config/databasedotcom.yml")
         client.authenticate :username => 'sampaul@berkeley.edu', :password => 'pleasedontkillme1TfPa8Eb0NneMsYA56IWaYDvd'
@@ -16,6 +30,7 @@ class LoginController < ApplicationController
         
         redirect_to root
 	end
+=end
 
 # Added Password field to Contact.
 # Connected App:        All users may self authorize, relax ip restrictions
