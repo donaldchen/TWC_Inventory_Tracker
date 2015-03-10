c3967662a6e7e4792dceb748f8c4c192d72d040c
SamPaul94 <samuelpaul7@gmail.com>
2015-03-09 20:26:07 -0700

    Merging

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,22 +1,26 @@
 class LoginController < ApplicationController
-	def index
-
+        def index
+	        # render template
+		# TODO change route to login / POST
+                # render "index"
+                render plain: "OK"
 	end
 
 	def login
-        client = Databasedotcom::Client.new("config/databasedotcom.yml")
-        client.authenticate :username => 'sampaul@berkeley.edu', :password => 'pleasedontkillme1TfPa8Eb0NneMsYA56IWaYDvd'
-        client.materialize("Contact")
-        contact = SFDC::Contact.find_by_email(email_address_field)
-        password = contact.Password__c
+                client = Databasedotcom::Client.new("config/databasedotcom.yml")
+                client.authenticate :username => 'sampaul@berkeley.edu', :password => 'pleasedontkillme1TfPa8Eb0NneMsYA56IWaYDvd'
+                client.materialize("Contact")
+                # contact = SFDC::Contact.find_by_email(email_address_field)
+                # actual_password = contact.Password__c
 
-        if password == password_field:
-        	# GRANT ACCESS
-        else:
-        	# DENY ACCESS
+         #        if actual_password == :password:
+         #        	# GRANT ACCESS
+         #        	# redirect_to_
+         #        else:
+         #        	# DENY ACCESS
+         #        	# flash, redirect to 
 	end
-end
-
 
 # Added Password field to Contact.
-# 
+# Connected App:        All users may self authorize, relax ip restrictions
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'login' => 'login#index'
+  get '/login' => 'login#index'
 
 
 
