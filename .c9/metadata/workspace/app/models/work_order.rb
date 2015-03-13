7985f81c3588cb81c92fbff184b1c4715cf46b6e
SamPaul94 <samuelpaul7@gmail.com>
2015-03-13 15:00:15 -0700

    Removed c9 files that fuck up the server

--- .c9/metadata/workspace/app/models/work_order.rb
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-{"filter":false,"title":"work_order.rb","tooltip":"/app/models/work_order.rb","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":0,"scrollleft":0,"selection":{"start":{"row":1,"column":1},"end":{"row":1,"column":1},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"timestamp":1426026384961,"hash":"3b95caf38400204fc1032750c94ab0c454f96761"}
-=======
-{"filter":false,"title":"work_order.rb","tooltip":"/app/models/work_order.rb","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":0,"scrollleft":0,"selection":{"start":{"row":0,"column":0},"end":{"row":0,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"timestamp":1426027490845,"hash":"3b95caf38400204fc1032750c94ab0c454f96761"}
->>>>>>> 7770f191b3204241051f461f9e72f988957ec988
