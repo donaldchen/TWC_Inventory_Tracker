bb036d59a0413476a7c7898d1f6d6626ba1ee105
plzdonkillme <robinhu510@gmail.com>
2015-03-14 04:36:13 +0000

    Made changes to web_steps

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -149,39 +149,11 @@ When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
   attach_file(field, File.expand_path(path))
 end
 
-Then /^(?:|I )should see "([^"]*)"$/ do |text|
+Then /^(?:|I )should( not)? see "([^"]*)"$/ do |n, text|
   if page.respond_to? :should
-    page.should have_content(text)
+    if n then page.should have_no_content(text) else page.should have_content(text) end
   else
-    assert page.has_content?(text)
-  end
-end
-
-Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
-  regexp = Regexp.new(regexp)
-
-  if page.respond_to? :should
-    page.should have_xpath('//*', :text => regexp)
-  else
-    assert page.has_xpath?('//*', :text => regexp)
-  end
-end
-
-Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_no_content(text)
-  else
-    assert page.has_no_content?(text)
-  end
-end
-
-Then /^(?:|I )should not see \/([^\/]*)\/$/ do |regexp|
-  regexp = Regexp.new(regexp)
-
-  if page.respond_to? :should
-    page.should have_no_xpath('//*', :text => regexp)
-  else
-    assert page.has_no_xpath?('//*', :text => regexp)
+    if n then assert page.has_no_content?(text) else assert page.has_content?(text) end
   end
 end
 
