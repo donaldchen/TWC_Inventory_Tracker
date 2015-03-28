dc452f83c65c60295376ba5a091d4da7e1a01b54
dcheng-id <davidcheng.id@gmail.com>
2015-03-28 15:58:01 -0700

    hi

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328211559) do
+ActiveRecord::Schema.define(version: 20150309230806) do
 
   create_table "inventories", force: :cascade do |t|
     t.string   "item"
