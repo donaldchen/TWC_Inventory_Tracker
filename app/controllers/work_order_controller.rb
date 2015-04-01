c6311b90c1a085ab63bd783a5fd9412d98933d22
Donald Chen <d.chen72@gmail.com>
2015-03-31 23:08:08 -0700

    fixes work order page

--- app/controllers/work_order_controller.rb
+++ app/controllers/work_order_controller.rb
@@ -14,7 +14,7 @@ class WorkOrderController < ApplicationController
 		 	flash[:notice] = "Invalid work order code"
 		 	redirect_to work_order_home_path
 		else
-			redirect_to item_list_path(WorkOrder.where(code: work_order_code).first)
+			redirect_to item_list_path(work_order_code)
 		end
 	end
 
