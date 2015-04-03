46275dd0fe19c4bff2877c62e65970af60c1b48b
dcheng-id <davidcheng.id@gmail.com>
2015-04-03 16:27:30 -0700

    2/5 Rspec done

--- app/controllers/store_item_controller.rb
+++ app/controllers/store_item_controller.rb
@@ -13,8 +13,11 @@ class StoreItemController < ApplicationController
 		end
 
 		if not flash[:notice]
+			item = Inventory.where(code: item_code).first
+			orig = item.quantity
 			item_name = Inventory.where(code: item_code).first.item.pluralize
 			item_name = item_name.slice(0,1).capitalize + item_name.slice(1..-1)
+			item.update(quantity: orig.to_i + quantity.to_i)
 			flash[:notice] = "#{quantity} #{item_name} Added"
 		end
 		redirect_to store_item_path
