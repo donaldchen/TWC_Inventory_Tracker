7c77c346fcb8a5b3936792cead022e3135f02a4d
SamPaul94 <samuelpaul7@gmail.com>
2015-04-13 15:57:00 -0700

    Salesforcified update quantity and update inventory

--- app/controllers/update_quantity_controller.rb
+++ app/controllers/update_quantity_controller.rb
@@ -1,22 +1,28 @@
 class UpdateQuantityController < ApplicationController
+	include Databasedotcom::Rails::Controller
 	def index
-		@items = Inventory.all
+		@items = Item__c.all
 	end
 
 	def update_item
-		@item = Inventory.where(code: params[:id]).first
-		flash[:code] = @item.code
+		# @item = Inventory.where(code: params[:id]).first
+		@item = Item__c.find_by_Code__c(params[:id])
+		flash[:code] = @item.Code__c
 	end
 
 	def update
-		correct_quantity = params[:item][:quantity]
+		correct_quantity = params[:item][:Quantity__c]
 		code = params[:item][:id]
+
+		p correct_quantity
+		p code
+		p "SDFSDKFJKJDSFKJKDSJFKDJFjf j fj jj  jj j j  "
 		if not correct_quantity.to_s =~ /^[0-9]+$/
 			flash[:notice] = "Quantity must be a number"
 			redirect_to update_item_path(code)
 		else
-			@item = Inventory.where(code: code).first
-			@item.quantity = correct_quantity
+			@item = Item__c.find_by_Code__c(code)
+			@item.Quantity__c = correct_quantity
 			@item.save
 			redirect_to update_quantity_path
 		end
